674bc31a38a2fec3dc665a09d5bd91f022d1cd7d
Bivrik <markvaselkof@yandex.ru>
2026-08-15 01:25:55 +0300

    Better inject in ScreenshotMixin using locals

--- common/src/main/java/net/bivrik/fancynotify/mixin/ScreenshotMixin.java
+++ common/src/main/java/net/bivrik/fancynotify/mixin/ScreenshotMixin.java
@@ -1,6 +1,5 @@
 package net.bivrik.fancynotify.mixin;
 
-import com.llamalad7.mixinextras.sugar.Local;
 import com.mojang.blaze3d.pipeline.RenderTarget;
 import com.mojang.blaze3d.platform.NativeImage;
 import net.bivrik.fancynotify.FancyNotify;
@@ -12,16 +11,21 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
 import java.io.File;
 import java.util.function.Consumer;
 
 @Mixin(Screenshot.class)
 public class ScreenshotMixin {
-    @Inject(method = "_grab", at = @At(
+    @Inject(method = "_grab",
+            at = @At(
                     value = "INVOKE_ASSIGN",
-                    target = "Lnet/minecraft/client/Screenshot;takeScreenshot(Lcom/mojang/blaze3d/pipeline/RenderTarget;)Lcom/mojang/blaze3d/platform/NativeImage;"))
-    private static void onScreenshotTaken(File gameDirectory, String screenshotName, RenderTarget buffer, Consumer<Component> messageConsumer, CallbackInfo info, @Local NativeImage nativeImage) {
+                    target = "Lnet/minecraft/client/Screenshot;takeScreenshot(Lcom/mojang/blaze3d/pipeline/RenderTarget;)Lcom/mojang/blaze3d/platform/NativeImage;"
+            ),
+            locals = LocalCapture.CAPTURE_FAILSOFT
+    )
+    private static void onScreenshotTaken(File gameDirectory, String screenshotName, RenderTarget buffer, Consumer<Component> messageConsumer, CallbackInfo info, NativeImage nativeImage) {
         NotificationManager manager = FancyNotify.getInstance().getNotificationManager();
         NativeImage imagePreview = new NativeImage(nativeImage.format(), nativeImage.getWidth(), nativeImage.getHeight(), false);
         imagePreview.copyFrom(nativeImage);
