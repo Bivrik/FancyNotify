e5689230cb5d2861c9fb199354b52ddbf4c60b21
Bivrik <markvaselkof@yandex.ru>
2026-08-26 12:57:29 +0300

    Fixed screenshot notification appearing when new world was created and entered

--- common/src/main/java/net/bivrik/fancynotify/mixin/ScreenshotMixin.java
+++ common/src/main/java/net/bivrik/fancynotify/mixin/ScreenshotMixin.java
@@ -1,8 +1,6 @@
 package net.bivrik.fancynotify.mixin;
 
-import com.mojang.blaze3d.buffers.GpuBuffer;
 import com.mojang.blaze3d.platform.NativeImage;
-import com.mojang.blaze3d.textures.GpuTexture;
 import net.bivrik.fancynotify.FancyNotify;
 import net.bivrik.fancynotify.notification.NotificationManager;
 import net.bivrik.fancynotify.notification.gui.ScreenshotNotification;
@@ -11,22 +9,20 @@ import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
 
+import java.awt.*;
+import java.io.File;
 import java.util.function.Consumer;
 
 @Mixin(Screenshot.class)
 public class ScreenshotMixin {
     // A bit fragile, I agree, but I didn't come up with anything better
-    @Inject(method = "lambda$takeScreenshot$1",
+    @Inject(method="lambda$grab$2",
             at = @At(
-                value = "INVOKE",
-                target = "Ljava/util/function/Consumer;accept(Ljava/lang/Object;)V"
-            ),
-            locals = LocalCapture.CAPTURE_FAILSOFT
-    )
-    private static void onScreenshotTaken(GpuBuffer buffer, int height, int downscaleFactor, int width, GpuTexture sourceTexture, Consumer<NativeImage> callback, CallbackInfo info,
-                          NativeImage image) {
+                    value = "INVOKE",
+                    target = "Lnet/minecraft/TracingExecutor;execute(Ljava/lang/Runnable;)V"
+    ))
+    private static void onScreenshotGrabbed(File workDir, String forceName, Consumer<Component> callback, NativeImage image, CallbackInfo info) {
         NotificationManager manager = FancyNotify.getInstance().getNotificationManager();
         if (manager != null) {
             NativeImage preview = new NativeImage(image.format(), image.getWidth(), image.getHeight(), false);
