5a18ae38ff42215b1e816dac424adf02da388a72
Bivrik <markvaselkof@yandex.ru>
2026-08-26 03:35:13 +0300

    Fix compatibility with Fancy Toasts

--- common/src/main/java/net/bivrik/fancynotify/mixin/ToastManagerMixin.java
+++ common/src/main/java/net/bivrik/fancynotify/mixin/ToastManagerMixin.java
@@ -1,15 +1,21 @@
 package net.bivrik.fancynotify.mixin;
 
 import net.bivrik.fancynotify.FancyNotify;
+import net.bivrik.fancynotify.accessor.IAdvancementHolderAccessor;
 import net.bivrik.fancynotify.core.Log;
 import net.bivrik.fancynotify.notification.NotificationManager;
 import net.minecraft.client.gui.GuiGraphicsExtractor;
+import net.bivrik.fancynotify.notification.gui.AdvancementNotification;
+import net.minecraft.advancements.DisplayInfo;
+import net.minecraft.client.gui.components.toasts.AdvancementToast;
 import net.minecraft.client.gui.components.toasts.Toast;
 import net.minecraft.client.gui.components.toasts.ToastManager;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+
+import java.util.Optional;
 
 @Mixin(ToastManager.class)
 public class ToastManagerMixin {
@@ -20,6 +26,12 @@ public class ToastManagerMixin {
     // the situation.
     @Inject(at = @At("HEAD"), method = "addToast", cancellable = true)
     private void onAddedToast(Toast toast, CallbackInfo info) {
+        if (toast instanceof AdvancementToast advancementToast) {
+            Optional<DisplayInfo> optionalDisplay = ((IAdvancementHolderAccessor) advancementToast).getAdvancementHolder().value().display();
+            NotificationManager manager = FancyNotify.getInstance().getNotificationManager();
+            optionalDisplay.ifPresent(displayInfo -> manager.add(new AdvancementNotification(manager, displayInfo.getTitle(), displayInfo.getType(), displayInfo.getIcon().create())));
+        }
+
         if (toast != null) {
             Log.info("Registered unsupported toast. Using vanilla toast system for {}", toast.getClass().getSimpleName());
         } else {
