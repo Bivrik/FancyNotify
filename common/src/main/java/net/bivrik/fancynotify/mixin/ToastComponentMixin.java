71053c1c3facff3a4aa2aef12e4fcf75d8af735a
Bivrik <markvaselkof@yandex.ru>
2026-08-26 04:30:15 +0300

    Tweaked ToastComponentMixin to make it a bit more descriptive and compatible

--- common/src/main/java/net/bivrik/fancynotify/mixin/ToastComponentMixin.java
+++ common/src/main/java/net/bivrik/fancynotify/mixin/ToastComponentMixin.java
@@ -17,13 +17,13 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 
 import java.util.Optional;
 
-@Mixin(ToastComponent.class)
+@Mixin(value = ToastComponent.class, priority = 1500)
 public class ToastComponentMixin {
-    // Mixin into the entry point of vanilla toasts, because there are a lot of static
-    // classes. All the modded ones will go through here, and if we don't catch it
-    // here it will just render as usual toast. Better compatibility!
-    // In the future just add instanceof to catch them or mixin as well, depends on
-    // the situation.
+    // Entrypoint of most vanilla toasts, but since in vanilla there
+    // are no expendable toasts, there are a lot of static addOrUpdate()
+    // methods. Therefore, here we only catch simple toasts, like
+    // advancements or from different mods. Every other toast
+    // is handled by vanilla system, therefore better compatibility!
     @Inject(at = @At("HEAD"), method = "addToast", cancellable = true)
     private void onAddedToast(Toast toast, CallbackInfo info) {
         if (toast instanceof AdvancementToast advancementToast) {
@@ -36,10 +36,11 @@ public class ToastComponentMixin {
             Log.info("Registered unsupported toast. Using vanilla toast system for {}", toast.getClass().getSimpleName());
         } else {
             info.cancel();
-            Log.error("Ugh... null toast?");
+            Log.warn("Ugh... null toast?");
         }
     }
 
+    // Clears all the toasts and notifications when leaving world
     @Inject(at = @At("HEAD"), method = "clear")
     private void onCleared(CallbackInfo info) {
         FancyNotify.getInstance().getNotificationManager().clear();
