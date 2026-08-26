f453353dc58addda355f2d6ff264e144ace77952
Bivrik <markvaselkof@yandex.ru>
2026-08-26 04:19:27 +0300

    Fixed SystemToast appearing after creating backup for world

---
+++ common/src/main/java/net/bivrik/fancynotify/mixin/EditWorldScreenMixin.java
@@ -0,0 +1,19 @@
+package net.bivrik.fancynotify.mixin;
+
+import net.minecraft.client.gui.components.toasts.Toast;
+import net.minecraft.client.gui.components.toasts.ToastComponent;
+import net.minecraft.client.gui.screens.worldselection.EditWorldScreen;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Redirect;
+
+@Mixin(EditWorldScreen.class)
+public class EditWorldScreenMixin {
+    @Redirect(method = "makeBackupAndShowToast", at = @At(
+            value = "INVOKE",
+            target = "Lnet/minecraft/client/gui/components/toasts/ToastComponent;addToast(Lnet/minecraft/client/gui/components/toasts/Toast;)V"
+    ))
+    private static void onBackupMadeAndToastShown(ToastComponent toastComponent, Toast toast) {
+        // Disable toast from creating
+    }
+}
