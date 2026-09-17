66c29679b3f8f55ef631547b4f874cb59854cccc
Bivrik <markvaselkof@yandex.ru>
2026-09-18 00:31:17 +0300

    Fixed NPE crash with Pufferfish's Skills SystemToast wrapper

--- common/src/main/java/net/bivrik/fancynotify/mixin/ToastComponentMixin.java
+++ common/src/main/java/net/bivrik/fancynotify/mixin/ToastComponentMixin.java
@@ -11,6 +11,7 @@ import net.minecraft.client.gui.components.toasts.AdvancementToast;
 import net.minecraft.client.gui.components.toasts.Toast;
 import net.minecraft.client.gui.components.toasts.ToastComponent;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Unique;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
@@ -19,6 +20,9 @@ import java.util.Optional;
 
 @Mixin(value = ToastComponent.class, priority = 9000)
 public class ToastComponentMixin {
+    @Unique
+    private static final String SIMPLE_TOAST = "net.puffish.skillsmod.client.gui.SimpleToast";
+
     // Entrypoint of most vanilla toasts, but since in vanilla there
     // are no expendable toasts, there are a lot of static addOrUpdate()
     // methods. Therefore, here we only catch simple toasts, like
@@ -26,6 +30,11 @@ public class ToastComponentMixin {
     // is handled by vanilla system, therefore better compatibility!
     @Inject(at = @At("HEAD"), method = "addToast", cancellable = true)
     private void onAddedToast(Toast toast, CallbackInfo info) {
+        if (toast == null) { // already handled
+            info.cancel();
+            return;
+        }
+
         NotificationManager manager = FancyNotify.getInstance().getNotificationManager();
         if (manager == null) {
             return;
@@ -42,12 +51,12 @@ public class ToastComponentMixin {
             return;
         }
 
-        if (toast != null) {
-            Log.info("Registered unsupported toast. Using vanilla toast system for {}", toast.getClass().getSimpleName());
-        } else {
+        if (toast.getClass().getName().equals(SIMPLE_TOAST)) {
             info.cancel();
-            Log.warn("Ugh... null toast?");
+            return;
         }
+
+        Log.info("Registered unsupported toast. Using vanilla toast system for {}", toast.getClass().getSimpleName());
     }
 
     // Clears all the toasts and notifications when leaving world
