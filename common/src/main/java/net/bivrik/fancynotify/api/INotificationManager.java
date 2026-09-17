d0b70ab8579e46268f1a834f05f08669adca6075
Bivrik <markvaselkof@yandex.ru>
2026-09-18 00:40:19 +0300

    Fixed NotificationManager API, but still not finished

--- common/src/main/java/net/bivrik/fancynotify/api/INotificationManager.java
+++ common/src/main/java/net/bivrik/fancynotify/api/INotificationManager.java
@@ -3,6 +3,8 @@ package net.bivrik.fancynotify.api;
 import net.minecraft.client.gui.GuiGraphics;
 
 public interface INotificationManager extends IClientNotificationManager {
+
     void update();
-    void render(GuiGraphics graphics, float partialTick);
+
+    void render(GuiGraphics graphics);
 }
