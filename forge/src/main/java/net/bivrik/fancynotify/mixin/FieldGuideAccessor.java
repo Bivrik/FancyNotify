7fda57247b8fbfe4cd91b8ee35f8f86ef1f637f9
Bivrik <markvaselkof@yandex.ru>
2026-09-20 15:27:01 +0300

    Porting to 1.20.1

---
+++ forge/src/main/java/net/bivrik/fancynotify/mixin/FieldGuideAccessor.java
@@ -0,0 +1,15 @@
+package net.bivrik.fancynotify.mixin;
+
+import com.evandev.fieldguide.client.gui.toasts.FieldGuideToast;
+import net.bivrik.compat.fieldguide.FieldGuideToastAccessor;
+import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.gen.Accessor;
+
+@Mixin(FieldGuideToast.class)
+public abstract class FieldGuideAccessor implements FieldGuideToastAccessor {
+    @Accessor("variantId")
+    public abstract String getVariantId();
+
+    @Accessor("entry")
+    public abstract Object getEntry();
+}
