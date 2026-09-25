eae197b6aba084af474e00e3161c0a3d8c29f6ba
Bivrik <markvaselkof@yandex.ru>
2026-09-25 03:06:06 +0300

    More robust Screenshot notification with dynamic textures

--- common/src/main/java/net/bivrik/fancynotify/gui/notification/ScreenshotNotification.java
+++ common/src/main/java/net/bivrik/fancynotify/gui/notification/ScreenshotNotification.java
@@ -1,7 +1,6 @@
 package net.bivrik.fancynotify.gui.notification;
 
 import com.mojang.blaze3d.platform.NativeImage;
-import net.bivrik.fancynotify.FancyNotify;
 import net.bivrik.fancynotify.api.Notification;
 import net.bivrik.fancynotify.api.NotificationGraphics;
 import net.bivrik.fancynotify.core.Constants;
@@ -12,23 +11,23 @@ import net.minecraft.network.chat.Component;
 import net.minecraft.resources.ResourceLocation;
 
 import java.awt.*;
-import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
 
-public class ScreenshotNotification extends FancyNotification {
-    private static final Random RANDOM = new Random();
-    private static final Component TITLE = Component.translatable("fancynotify.gui.screenshot.title");
+public class ScreenshotNotification extends FancyExpandableNotification {
     private static final ResourceLocation BACKGROUND = ResourceLocations.of("notifications/screenshot");
     private static final ResourceLocation SCREENSHOT_PREVIEW = ResourceLocations.of("screenshot_preview");
-    public static final int TITLE_COLOR = new Color(43, 181, 43).getRGB();
+
+    private static final Component TITLE = Component.translatable("fancynotify.gui.screenshot.title");
+    private static final int TITLE_COLOR = new Color(43, 181, 43).getRGB();
 
     private final TextureManager textureManager;
+    private final DynamicTexture dynamicScreenshotTexture;
 
     public ScreenshotNotification(NativeImage screenshotImage) {
-        super(TITLE, Component.translatable(Constants.MOD_ID + ".gui.screenshot." + RANDOM.nextInt(3)));
+        super(TITLE, Component.translatable(Constants.MOD_ID + ".gui.screenshot." + ThreadLocalRandom.current().nextInt(3)));
 
         this.textureManager = this.minecraft.getTextureManager();
-        DynamicTexture screenshotPreview = new DynamicTexture(screenshotImage);
-        this.textureManager.register(SCREENSHOT_PREVIEW, screenshotPreview);
+        this.dynamicScreenshotTexture = new DynamicTexture(screenshotImage);
     }
 
     @Override
@@ -37,15 +36,33 @@ public class ScreenshotNotification extends FancyNotification {
     }
 
     @Override
-    public void onRemoval() {
-        super.onRemoval();
-
-        textureManager.release(SCREENSHOT_PREVIEW);
+    public int getTextOffset() {
+        return super.getTextOffset() + 17;
     }
 
     @Override
-    public int getTextOffset() {
-        return super.getTextOffset() + 17;
+    public void onShowing() {
+        textureManager.register(SCREENSHOT_PREVIEW, dynamicScreenshotTexture);
+    }
+
+    @Override
+    public void onRemoval() {
+        textureManager.release(SCREENSHOT_PREVIEW);
+        if (dynamicScreenshotTexture != null) {
+            dynamicScreenshotTexture.close();
+        }
+    }
+
+    @Override
+    public void expand(Notification expansion) {
+        ScreenshotNotification other = (ScreenshotNotification) expansion;
+
+        NativeImage newScreenshotImage = other.dynamicScreenshotTexture.getPixels();
+        NativeImage screenshotImage = dynamicScreenshotTexture.getPixels();
+        if (newScreenshotImage != null && screenshotImage != null) {
+            screenshotImage.copyFrom(newScreenshotImage);
+            dynamicScreenshotTexture.upload();
+        }
     }
 
     @Override
@@ -55,6 +72,10 @@ public class ScreenshotNotification extends FancyNotification {
         graphics.multiline(getWrappedMessage(), getTextOffset(), 18, -1);
         int width = 38;
         int height = 22;
-        graphics.texture(SCREENSHOT_PREVIEW, 5, getCenterY() - height / 2, width, height, width * 4, height * 4, ((width * 4) - width) / 2, ((height * 4) - height) / 2);
+        graphics.texture(SCREENSHOT_PREVIEW,
+                5, getCenterY() - height / 2,
+                width, height,
+                width * 4, height * 4,
+                ((width * 4) - width) / 2, ((height * 4) - height) / 2);
     }
 }
